4998c3a3739a0c3b2eed5fbedeab0e9f18330a74
Asher <haaris@trainingbasket.in>
2023-06-05 17:31:26 +0530

    Prodcuts display is done

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import './productCard.css'
 
-export default function ProductCard() {
+export default function ProductCard(props) {
+  const {name, image, category, oldPrice, newPrice, discount, rating} = props
+  console.log(image);
   return (
     <div className="mb-4 card shadow-sm">
-      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+      <img className='thumbnail' src={image} alt="" />
       <div className="card-body">
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h4>{name}</h4>
+        <h6 className='text-success'>{category}</h6>
+        <div className="price-section d-flex align-items-center">
+          <span className='new_price'>Rs. {newPrice}/-</span>
+          <span className='old_price'><del>Rs. {oldPrice}</del></span>
+          <span className='discount'>({discount}%)</span>
+        </div>
         <div className="d-flex justify-content-between align-items-center">
           <div className="btn-group">
-            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+            <button type="button" className="btn btn-outline-primary"><i class="fa-solid fa-cart-shopping"></i></button>
+            <button type="button" className="btn btn-outline-danger"><i class="fa-solid fa-heart"></i></button>
           </div>
-          <small className="text-body-secondary">9 mins</small>
+          <small className="text-body-secondary"><i className="fa-solid fa-star"></i>&nbsp;{rating}</small>
         </div>
       </div>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductCard from '../components/ProductCard/ProductCard'
 import Sidebar from '../components/Sidebar/Sidebar'
 import Footer from '../components/Footer/Footer'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export default function HomePage() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // request data from data.json file
+        const { data: { books } } = await axios.get("data.json")
+        // console.log(books);
+        setProducts(books)
+      }
+      catch (error) {
+        // handle the error
+        console.log(error);
+      }
+    })()
+  }, [])
+
+
+  //IIFE - Instantly Invoked function Expression 
   return (
     <div>
       <div className="container-fluid">
@@ -13,21 +34,13 @@ export default function HomePage() {
           </div>
           <div className="col-md-9">
             <div className="row py-4 products">
-              <div className="col-md-4">
-                <ProductCard />
-              </div>
-              <div className="col-md-4">
-                <ProductCard />
-              </div>
-              <div className="col-md-4">
-                <ProductCard />
-              </div>
-              <div className="col-md-4">
-                <ProductCard />
-              </div>
-              <div className="col-md-4">
-                <ProductCard />
-              </div>
+              {
+                products.map((product) => {
+                  return <div className="col-md-4" key={product.id}>
+                    <ProductCard name={product.name} image={product.image} category={product.category} oldPrice={product.oldPrice} newPrice={product.newPrice} discount={product.discount} rating={product.rating} />
+                  </div>
+                })
+              }
             </div>
           </div>
         </div>
@@ -36,6 +49,3 @@ export default function HomePage() {
     </div>
   )
 }
-
-
-// Name | Rating | Category | Price | Discounted price | Image
